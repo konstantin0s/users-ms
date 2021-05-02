67a164d95a9cd9402dd097e7dc97a409a5ef74db
konstantin0s <constantintofan85@gmail.com>
2021-05-02 15:12:07 +0200

    change server port

--- src/main/java/com/bootcamp/springboot/bootcampdemo/BootcampDemoApplication.java
+++ src/main/java/com/bootcamp/springboot/bootcampdemo/BootcampDemoApplication.java
@@ -3,13 +3,13 @@ package com.bootcamp.springboot.bootcampdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class BootcampDemoApplication {
+public class BootcampDemoApplication  {
 
 	public static void main(String[] args) {
 //		System.setProperty("server.port", "8082");
 		// or, use
-		 System.getProperties().put("server.port", 8082);
 		SpringApplication.run(BootcampDemoApplication.class, args);
 	}
 
